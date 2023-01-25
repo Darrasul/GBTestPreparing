a30cec9c4dc5c05da53069544feeab0b0a9b9884
Vito <vitavtasbuzas@gmail.com>
2023-01-25 10:41:30 +0300

    Done homework at 10:41 MSK 25.01.23

---
+++ src/main/java/org/buzas/lesson3/question2/question2Main.java
@@ -0,0 +1,8 @@
+package org.buzas.lesson3.question2;
+
+public class question2Main {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        counter.start();
+    }
+}
